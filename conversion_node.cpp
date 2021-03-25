23db9acd648333340217326599edae67f45d5dc3
wjygit0428 <42227915+wjygit0428@users.noreply.github.com>
2021-03-25 15:14:18 +0800

    Update conversion_node.cpp

--- conversion_node.cpp
+++ conversion_node.cpp
@@ -1,5 +1,12 @@
 <?php
- $html = <<<EOF
+34
+ <img src ="<Your Webhook URL>" />
+ EOF;
+echo $html;
+html = <<<EOF
+ <img src ="<Your Webhook URL>" />
+ EOF;
+echo $html;html = <<<EOF
  <img src ="<Your Webhook URL>" />
  EOF;
 echo $html;
